9920c97156f19e2ecde801c73e1e08f149245e03
Jose Arenas <jose@aulaformativa.com>
2015-06-10 06:42:22 -0500

    Este es mi primer commit

---
+++ mensaje.php
@@ -0,0 +1,15 @@
+<?php  
+	$nombre = "Kevin Campo";
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<h1>Hola mi nombre es <?php echo $nombre ?></h1>
+<?php echo "Les escribo desde PHP" ?>
+</body>
+</html>
